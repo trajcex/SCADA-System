9fb049658f29d6510ef98609deacfecc660af901
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2024-06-18 22:23:37 +0200

    feat: Connected with CoreService

--- DatabaseManager/Program.cs
+++ DatabaseManager/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DatabaseManager.CoreService;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,12 @@ using System.Threading.Tasks;
 
 namespace DatabaseManager
 {
-    internal class Program
+    public class Program
     {
+        static UserClient service = new UserClient();
         static void Main(string[] args)
         {
+            service.RegisterUser("nikola", "123");
         }
     }
 }
