f04614641408accf54ac8fa869ed11f44b633180
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2024-06-19 20:22:33 +0200

    feat: Created RealTimeDriver

---
+++ CoreService/RealTimeDriver.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using CoreService.Interface;
+
+namespace CoreService
+{
+    public class RealTimeDriver : IRealTimeDriver
+    {
+        public void DoWork()
+        {
+            
+        }
+    }
+}
