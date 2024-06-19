5fe1a376f22d360a8858c657066d4bd7ec18a88f
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2024-06-19 20:23:33 +0200

    feat: Created InputValidetor

--- RealTimeUnit/Program.cs
+++ RealTimeUnit/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace RealTimeUnit
@@ -10,6 +11,17 @@ namespace RealTimeUnit
     {
         static void Main(string[] args)
         {
+            string uid = Guid.NewGuid().ToString();
+            (int lowLimit, int highLimit) = InputValidator.GetValidatedInteger();
+
+            Random random = new Random();   
+            while (true)
+            {
+                int value =  random.Next(lowLimit,highLimit);
+                Thread.Sleep(3000);
+            }
+          
+
         }
     }
 }
