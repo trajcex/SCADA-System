4041f6c4a57afca12aeb7140656b9598c1e44500
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2024-06-19 15:10:09 +0200

    feat: Added SaveTags and GetTAgs in ICore

--- CoreService/CoreService.svc.cs
+++ CoreService/CoreService.svc.cs
@@ -1,5 +1,7 @@
 ﻿using CoreService.Interface;
 using CoreService.Model;
+using CoreService.Repository;
+using CoreService.Repository.Interface;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity.Validation;
@@ -10,12 +12,12 @@ using System.Text;
 
 namespace CoreService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CoreService" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select CoreService.svc or CoreService.svc.cs at the Solution Explorer and start debugging.
     public class CoreService : ICore,IUser
     {
-        public void DoWork()
+        public List<Tag> GetTags()
         {
+            ITagRepository tagRepository = new TagRepository();
+            return tagRepository.GetTags()["DigitalInputTag"];
         }
 
         public void RegisterUser(string username, string password)
@@ -32,6 +34,39 @@ namespace CoreService
             }
         }
 
-        
+        public void SaveTags()
+        {
+            ITagRepository tagRepository = new TagRepository();
+            List<Tag> di = new List<Tag>
+            {
+                new DigitalInputTag("DI1", "Digital Input Tag 1", "Address1", "Driver1", 1, true),
+                new DigitalInputTag("DI2", "Digital Input Tag 2", "Address2", "Driver2", 1, false)
+            };
+
+                List<Tag> d0 = new List<Tag>
+            {
+                new DigitalOutputTag(100, "DO1", "Digital Output Tag 1", "Address1"),
+                new DigitalOutputTag(200, "DO2", "Digital Output Tag 2", "Address2")
+            };
+
+                List<Tag> ai = new List<Tag>
+            {
+                new AnalogInputTag("AI1", "Analog Input Tag 1", "Address1", new List<Alarm>(), "Driver1", 1, true, "Volts", 0, 100),
+                new AnalogInputTag("AI2", "Analog Input Tag 2", "Address2", new List<Alarm>(), "Driver2", 2, false, "Amps", 0, 50)
+            };
+
+                List<Tag> ao = new List<Tag>
+            {
+                new AnalogOutputTag("AO1", "Analog Output Tag 1", "Address1", 50, "Volts", 0, 100),
+                new AnalogOutputTag("AO2", "Analog Output Tag 2", "Address2", 75, "Amps", 0, 50)
+            };
+
+            Dictionary<string, List<Tag>> map = new Dictionary<string, List<Tag>>();
+            map.Add("DigitalInputTags", di);
+            map.Add("DigitalOutputTags", d0);
+            map.Add("AnalogInputTags", ai);
+            map.Add("AnalogOutputTags", ao);
+            tagRepository.SaveTags(map);
+        }
     }
 }
--- CoreService/Interface/ICore.cs
+++ CoreService/Interface/ICore.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CoreService.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -11,8 +12,11 @@ namespace CoreService.Interface
     [ServiceContract]
     public interface ICore
     {
+
         [OperationContract]
-        void DoWork();
+        void SaveTags();
+        [OperationContract]
+        List<Tag> GetTags();
     }
 
 
