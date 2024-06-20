0a5cf2fcb51dc3886cd53cd79a91d8c8d6b48d6c
Nikola Trajkovic <105116816+trajcex@users.noreply.github.com>
2024-06-20 18:02:04 +0200

    Revert "Feat: Tag processing"

--- Trending/Program.cs
+++ Trending/Program.cs
@@ -1,29 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
-using Trending.TagPorcessingServiceReference;
 
 namespace Trending
 {
     internal class Program
     {
-        public class Callback : IMonitoringCallback
-        {
-            public void Message(string message)
-            {
-                Console.WriteLine(message);
-            }
-        }
-        static MonitoringClient monitroing;
         static void Main(string[] args)
         {
-            InstanceContext ic = new InstanceContext(new Callback());
-            monitroing = new MonitoringClient(ic);
-            monitroing.InitSub();
-            Console.ReadKey();
         }
     }
 }
